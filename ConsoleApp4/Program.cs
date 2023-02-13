b9ff9b0284a83a4b82fe3b2f5714f5cd5393dae5
miguelangellr1 <miguelangellr1A@outlook.com>
2023-02-13 12:47:25 -0800

    Se agrega el Proyecto

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string enviaCorreos = "";
+            Console.WriteLine("Desea enviar un correo elecromico ?");
+            enviaCorreos = Console.ReadLine();
+            if(enviaCorreos == "si")
+            {
+                string servidor = "smtp.gmail.com";
+                int puerto = 587;
+                string gmailUser = "ml2297071@gmail.com";
+                string gmailPass = "miguelangel2004xdxd01";
+
+                MimeMessage mensaje = new MimeMessage();
+                mensaje.From.Add(new MailboxAddress("prueba01",gmailUser));
+                mensaje.To.Add(new MailboxAddress("destino",gmailUser));
+                mensaje.Subject = "Hola como estas Programador";
+
+                BodyBuilder cuerpoMensaje = new BodyBuilder();
+                cuerpoMensaje.HtmlBody = "hola <b>mundo</b>";
+
+                mensaje.Body = cuerpoMensaje.ToMessageBody();
+
+                SmtpClient clienteSmtp = new SmtpClient();
+                clienteSmtp.CheckCertificateRevocation = false;
+                clienteSmtp.Connect(servidor, puerto, MailKit.Security.SecureSocketOptions.StartTls);
+                clienteSmtp.Authenticate(gmailUser,gmailPass);
+                clienteSmtp.Send(mensaje);
+                clienteSmtp.Disconnect(true);
+
+            }
+            Console.WriteLine("correo enviado exitosamente");
+
+            Console.ReadLine();
+
+        }
+    }
+}
